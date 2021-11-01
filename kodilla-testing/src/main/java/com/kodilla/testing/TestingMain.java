284518c0d6e570279b54ca5f4d40b387ad5315d9
RafalJag <rafal.jagosz@gmail.com>
2021-11-01 15:55:39 +0100

    6.3 Added kodilla.testing.collection.OddNumbersExterminator + unit tests

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,25 +1,9 @@
 package com.kodilla.testing;
 
-import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.collection.OddNumbersExterminator;
 
 public class TestingMain {
 
     public static void main(String[] args) {
-        Calculator calculator = new Calculator(5,10);
-
-        int result1 = calculator.addAToB();
-        int result2 = calculator.subtractAFromB();
-
-        if (result1 == calculator.getA() + calculator.getB()) {
-            System.out.println("addAToB: test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        if (result2 == calculator.getB() - calculator.getA()) {
-            System.out.println("subtractAFromB: test OK");
-        } else {
-            System.out.println("Error!");
-        }
     }
 }
