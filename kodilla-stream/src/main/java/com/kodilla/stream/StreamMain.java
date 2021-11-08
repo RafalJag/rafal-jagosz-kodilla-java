4c5146e56c1ae37db9e779015267fd2bac084f13
RafalJag <rafal.jagosz@gmail.com>
2021-11-08 14:49:46 +0100

    7.1 fix

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -7,11 +7,11 @@ public class StreamMain {
 
     public static void main(String[] args) {
         PoemBeautifier textToEdt = new PoemBeautifier();
-        textToEdt.beautify("Tekst", "Xxx.", ".xxX",
+        textToEdt.decorate("Tekst", "Xxx.", ".xxX",
                 (text, start, end) -> start + text + end);
-        textToEdt.beautify("Tekst", "", "",
+        textToEdt.decorate("Tekst", "", "",
                 (text, start, end) -> start + text.toUpperCase() + end);
-        textToEdt.beautify("Tekst", "|____", "____|",
+        textToEdt.decorate("Tekst", "|____", "____|",
                 (text, start, end) -> start + text.toUpperCase() + end);
     }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -1,7 +1,7 @@
 package com.kodilla.stream.beautifier;
 
 public class PoemBeautifier {
-    public void beautify(String textToDecorate, String start, String end, PoemDecorator text){
+    public void decorate(String textToDecorate, String start, String end, PoemDecorator text){
         String result = text.decorate(textToDecorate, start, end);
         System.out.println(result);
     }
