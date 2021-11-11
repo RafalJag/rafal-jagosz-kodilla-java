24a9049626ddf0d558d9d5d418c4e8b8c36e5711
RafalJag <rafal.jagosz@gmail.com>
2021-11-11 04:05:05 +0100

    7.5 Added .stream.array + test

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -10,16 +10,12 @@ public class ArrayOperationsTestSuite {
     void testGetAverage() {
 
         //Given
-
         int[] numbersTest = {1, 12, 23, 34, 45, 56, 67, 78, 89, 90, 110, 121, 132, 143, 154, 165, 176, 187, 198, 200 };
 
         //When
-
         double averageTest = ArrayOperations.getAverage(numbersTest);
 
         //Then
-
         assertEquals(104.05, averageTest);
-
     }
 }
