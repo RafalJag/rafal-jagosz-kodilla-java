167dd58b5b58fb06d1d2515169a6aa3ecf0625ef
RafalJag <rafal.jagosz@gmail.com>
2021-11-12 19:36:47 +0100

    7.6 Added .stream.portfolio test

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -6,9 +6,14 @@ public interface ArrayOperations {
 
     static double getAverage(int[] numbers) {
 
-        IntStream.range(0, numbers.length).map(s -> numbers[s]).forEach(System.out::println);
-        IntStream.range(0, numbers.length).map(s -> numbers[s]).average().getAsDouble();
-        return IntStream.range(0, numbers.length).map(s -> numbers[s]).average().getAsDouble();
+        IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .forEach(System.out::println);
+        IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .average().getAsDouble();
+        return IntStream.range(0, numbers.length)
+                .map(n -> numbers[n]).average().getAsDouble();
 
     }
 }
