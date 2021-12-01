757797b7077b695e7f09b7a0bf1a48584dbe9ccc
RafalJag <rafal.jagosz@gmail.com>
2021-12-01 17:19:07 +0100

    14.3 Added spring.portfolio + test

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -0,0 +1,27 @@
+package com.kodilla.spring.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
+public class BoardTestSuite {
+
+    @Test
+    void testTaskAdd(){
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+        Board board = context.getBean(Board.class);
+
+        //When
+        board.getToDoList().getTasks().add("Zadanie - do zrobienia");
+        board.getInProgressList().getTasks().add("Zadanie - w trakcie");
+        board.getDoneList().getTasks().add("Zadanie - zakończone");
+
+        //Then
+        System.out.println(board.getToDoList().getTasks().get(0));
+        System.out.println(board.getInProgressList().getTasks().get(0));
+        System.out.println(board.getDoneList().getTasks().get(0));
+    }
+}
